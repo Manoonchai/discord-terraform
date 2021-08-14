c8f41919d75332ecff9bf43eff70be1cb56c6ada
Manassarn Manoonchai <manassarn@gmail.com>
2021-08-14 14:14:25 +0700

    Cleanup, generate invite link

--- main.tf
+++ main.tf
@@ -163,47 +163,6 @@ resource discord_voice_channel meeting_room {
 //   position = 1
 // }
 
-// resource discord_category_channel public {
-//   name = "public"
-//   position = 0
-//   server_id = data.discord_server.deezcord_server.id
-// }
-
-// resource discord_category_channel members {
-//   name = "members"
-//   position = 0
-//   server_id = data.discord_server.deezcord_server.id
-// }
-
-// resource discord_text_channel welcome {
-//   name = "welcome"
-//   topic = "The welcome channel"
-//   position = 0
-//   category = discord_category_channel.public.id
-//   server_id = data.discord_server.deezcord_server.id
-// }
-
-// resource discord_text_channel gaming {
-//   name = "gaming"
-//   topic = "The Gaming channel"
-//   position = 1
-//   category = discord_category_channel.members.id
-//   server_id = data.discord_server.deezcord_server.id
-// }
-
-// resource discord_text_channel bots {
-//   name = "bots"
-//   topic = "The bots channel"
-//   position = 2
-//   category = discord_category_channel.members.id
-//   server_id = data.discord_server.deezcord_server.id
-// }
-
-// resource discord_invite welcome {
-//   channel_id = discord_text_channel.welcome.id
-//   max_age = 3600
-// }
-
 // resource discord_member_roles admin {
 //   count = var.admin_user_invited ? 1 : 0
 //   user_id = data.discord_member.admin.id
@@ -217,8 +176,13 @@ resource discord_voice_channel meeting_room {
 //   // }
 // }
 
-// output invite {
-//   value = "https://discord.gg/${discord_invite.welcome.id}"
-// }
+// Invitation link
 
-// Pulling existing resource with terraform import [resource] [ID]
+resource discord_invite welcome {
+  channel_id = discord_text_channel.welcome.id
+  max_age = 3600
+}
+
+output invite {
+  value = "https://discord.gg/${discord_invite.welcome.id}"
+}
